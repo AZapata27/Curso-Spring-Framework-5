231a8a01fe6d714a71727459d749581d90bab62f
Andres Zapata <azapata@uc.edu.co>
2021-04-29 13:42:53 -0500

    Se agrega arquitectura MVC con Hibernate JPA

--- SpringBoot-MVC/src/main/java/com/example/demo/controllers/IndexController.java
+++ SpringBoot-MVC/src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,19 +1,42 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.models.Persona;
 
 @Controller
 public class IndexController {
 
 	
 	
-	@RequestMapping({"/index","/","/home"})
+	@GetMapping({"/index","/","","/home"})
 	public String index(ModelMap model){
 		model.addAttribute("titulo", "Hola Spring Framework");
+
+		Persona persona = new Persona();
+		persona.setNombre("juan");
+		persona.setApellido("perez");
+		persona.setEmail("jperez@mail.com");
+		persona.setTelefono("3118571088");
+
+		Persona persona2 = new Persona();
+		persona2.setNombre("karmenza");
+		persona2.setApellido("hermeleginda");
+		persona2.setEmail("kherme@mail.com");
+		persona2.setTelefono("3147723109");
+
+		List<Persona> personas = new ArrayList<>();
+		personas.add(persona);
+		personas.add(persona2);
+		
+		model.addAttribute("personas",personas);
+
 		return "index";
 	}
+	
 }
---
+++ SpringBoot-MVC/src/main/java/com/example/demo/models/Persona.java
@@ -0,0 +1,18 @@
+package com.example.demo.models;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	
+
+	
+	
+	
+	
+}
