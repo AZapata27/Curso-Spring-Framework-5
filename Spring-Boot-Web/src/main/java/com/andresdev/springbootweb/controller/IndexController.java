76babebdaea6d05fcadc4f865203af86359deac0
Andres Zapata <afzapataa@indracompany.com>
2021-05-26 12:30:07 -0500

    creacion index controller

--- Spring-Boot-Web/src/main/java/com/andresdev/springbootweb/controller/IndexController.java
+++ Spring-Boot-Web/src/main/java/com/andresdev/springbootweb/controller/IndexController.java
@@ -1,4 +1,17 @@
 package com.andresdev.springbootweb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+
+    @GetMapping(value = {"/index","/","/home"})
+    public String index(ModelMap model){
+
+    model.addAttribute("title","Titulo Hola Spring");
+
+        return "index";
+    }
 }
