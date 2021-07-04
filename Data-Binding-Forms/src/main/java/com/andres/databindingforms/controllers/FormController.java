a86db7ed58b7f99398c91a5d8e2da725dcc3456b
Andres Zapata <afzapataa@indracompany.com>
2021-07-04 15:25:06 -0500

    binding por medio de @request params

--- Data-Binding-Forms/src/main/java/com/andres/databindingforms/controllers/FormController.java
+++ Data-Binding-Forms/src/main/java/com/andres/databindingforms/controllers/FormController.java
@@ -1,4 +1,28 @@
 package com.andres.databindingforms.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class FormController {
+
+    @GetMapping("/form")
+    public String Form(Model model){
+        model.addAttribute("titulo", "Formulario");
+        return "form1";
+    }
+
+    @PostMapping("/form")
+    public String procesar(Model model, @RequestParam String username,@RequestParam String password, @RequestParam String email){
+
+
+        model.addAttribute("titulo", "Resultado del formulario");
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
+        return "resultado1";
+    }
 }
